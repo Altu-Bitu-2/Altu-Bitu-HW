cee124f8366d25d61565530bdb99378fdf0c0c13
woongjichoi <71050595+woongjichoi@users.noreply.github.com>
2022-03-11 22:34:51 +0900

    [정렬] 3월 11일 - Update

--- 03월 04일 - 정렬/10804.cpp
+++ 03월 04일 - 정렬/10804.cpp
@@ -5,7 +5,10 @@
 using namespace std;
 
 int main(){
-	vector<int> card={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+	vector<int> card(20);
+	for (int i=0; i<20; i++){
+		card[i]=i+1;
+	}
 	int start, end;
 
 	for (int i=0; i<10; i++){
