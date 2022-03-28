93f039326ca5fa9b9362b43a279447b25cc2f88e
woongjichoi <71050595+woongjichoi@users.noreply.github.com>
2022-03-28 23:06:12 +0900

    [구현 & 코너케이스] 3월 28일 - Update

--- 03월 22일 - 구현 & 코너케이스/11723.py
+++ 03월 22일 - 구현 & 코너케이스/11723.py
@@ -1,44 +1,43 @@
-# 1. s를 리스트로 구현 -> 메모리 초과
-# 2. 힌트 참고 -> 모르겠음.. 
-# 3. 구글링 -> s를 set으로 구현하고 add와 discard 사용하여 if문 줄임 -> 메모리 초과
+# 메모리 초과 해결 위해서는 True, False로 저장해주는 아이디어 말고도
+# 코드 효율적으로도 수정해야 함 (https://www.acmicpc.net/submit/11723/41132196)
+
+# 더 빨리 input 받기 
+import sys
+input=sys.stdin.readline
 
 n=int(input())
-operations=[]
+
+check=[False]*20 # 있으면 True, 없으면 False
+
 for i in range(n):
-    operations.append(input())
-
-s=set([])
-
-for operation in operations: 
+    operation=input()
+    
     a=(operation.split())[0] # 수행해야 할 연산의 종류
     
     if a=="all":
-        s=set(['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'])
+        check=[True]*20
         continue
     
     elif a=="empty":
-        s=set([])
+        check=[False]*20
         continue
         
-    elif a=="add" or a=="remove" or a=="check" or a=="toggle":
-        b=(operation.split())[1] # x -> str로 받아진 것 주의! 
+    b=(operation.split())[1] # x -> str로 받아진 것 주의! 
 
     if a=="add":
-        #if b not in s:
-        s.add(b)
+        check[int(b)-1]=True
 
     elif a=="remove":
-        #if b in s:
-        s.discard(b) # discard 쓰는 이유: remove와 달리 discard는 없어도 정상 종료
-
+        check[int(b)-1]=False
+        
     elif a=="check":
-        if b in s:
+        if check[int(b)-1]==True:
             print(1)
         else:
             print(0)
 
     elif a=="toggle":
-        if b in s:
-            s.discard(b)
+        if check[int(b)-1]==True:
+            check[int(b)-1]=False
         else:
-            s.add(b)
+            check[int(b)-1]=True
