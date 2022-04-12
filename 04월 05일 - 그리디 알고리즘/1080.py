017cf90ef529ab305d97fd6d231aee84b330e49c
woongjichoi <71050595+woongjichoi@users.noreply.github.com>
2022-04-12 18:17:28 +0900

    [그리디 알고리즘] 4월 12일 - Update

--- 04월 05일 - 그리디 알고리즘/1080.py
+++ 04월 05일 - 그리디 알고리즘/1080.py
@@ -13,19 +13,14 @@ answer=0
 def changeArray(a, x, y):
     for i in range(x, x+3):
         for j in range(y, y+3):
-            if a[i][j]==1:
-                a[i][j]=0
-            else:
-                a[i][j]=1
+            a[i][j]=1-a[i][j]
 
 def checkArray(a, b):
-    flag=1
     for i in range(n):
         for j in range(m):
             if a[i][j]!=b[i][j]:
-                flag=0
-                break
-    return flag
+                return 0
+    return 1
 
 for i in range(n-2): 
     for j in range(m-2): 
